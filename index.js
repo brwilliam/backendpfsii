265452ed09053f37efdbb46bdb3b73b9743cd0c0
Bruno William <brunowilliam997@gmail.com>
2024-03-17 16:27:58 -0300

    Get Pedido

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import rotaRestaurante from "./Rotas/rotaRestaurante.js";
 import rotaPedido from "./Rotas/rotaPedido.js"; 
 import rotaLogin from "./Rotas/rotaLogin.js";
 import rotaGarcom from "./Rotas/rotaGarcom.js";
-import rotaPedidoGarcomMesa from "./Rotas/rotaPedidoGarcomMesa.js"; // Importe a rota do pedido garcom mesa
 import dotenv from "dotenv";
 import session from "express-session";
 import { verificarAcesso } from "./Seguranca/Autenticacao.js";
@@ -34,7 +33,6 @@ app.use("/login", rotaLogin);
 app.use("/restaurante", verificarAcesso, rotaRestaurante); // Use a rota do restaurante
 app.use("/pedido", rotaPedido); // Use a rota do pedido
 app.use("/garcom",verificarAcesso, rotaGarcom);
-app.use("/pedido-garcom-mesa",verificarAcesso, rotaPedidoGarcomMesa); // Adicione a rota do pedido garcom mesa
 
 app.listen(porta, host, () => {
   console.log(`Servidor escutando na porta ${host}:${porta}.`);
